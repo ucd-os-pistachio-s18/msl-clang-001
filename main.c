8d071df7e1e878914b7e8fae275248a9072f4e80
tb52s7 <TrogPhone@gmail.com>
2018-01-18 07:38:16 -0700

    successful compilation and updated .gitignore

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+
+    printf("DEBUG:  Program has compiled successfully");
 
     exit(0);
 }
