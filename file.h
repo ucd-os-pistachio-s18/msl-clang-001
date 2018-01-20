d711bbb038d8426a735b439ee005558e47272dcd
C5T2B7U <brian.sumner@ucdenver.edu>
2018-01-19 19:32:51 -0700

    trimmed deprecated file functions

--- file.h
+++ file.h
@@ -20,23 +20,6 @@ void processFiles(const char* inputFilename, const char* outputFilename, const i
 void getWords(FILE *file_ptr, const int BUFFER_SIZE, const int SHOW_DEBUG);
 
 
-/* FUNCTION:  openFile
- * RECEIVES:  FILE POINTER, FILENAME, IO MODE
- * RETURNS:   VOID
- * PERFORMS:  TRIES TO OPEN FILE; PRINTS USAGE AND EXITS IF FILE NOT OPEN
- * NOTE:      DO NOT USE THIS FUNCTION
- */
-void openFile(FILE *file_ptr, char* fileName, char* ioType);
-
-
-/* FUNCTION:  closeFile
- * RECEIVES:  FILE POINTER
- * RETURNS:   VOID
- * PERFORMS:  CLOSES FILE
- * NOTE:      CRASHES.  DO NOT USE THIS FUNCTION
- */
-void closeFile(FILE *file_ptr);
-
 
 
 
